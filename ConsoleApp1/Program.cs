3235ab347ad8c8b95fd613c16a92ee2447fc4899
zhanghpp <zhanghpp@dcholdings.com>
2021-12-21 11:48:51 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string path = @"D:\Sync\Work\";
+
+            var file = new FileUpdateMonitoring(path);
+            file.FileLists();
+
+
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+
+
+                file.GetLastWriteFile();
+
+            } while (true);
+        }
+    }
+}
