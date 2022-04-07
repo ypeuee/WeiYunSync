24d41350ab015fa894b9e073c240d6ccd8bb48c4
zhanghpp <zhanghpp@dcholdings.com>
2022-04-07 14:04:03 +0800

    add-添加扫描页面

---
+++ MainApp/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using RadarControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random(DateTime.Now.Millisecond);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 鼠标左键拖动
+            MouseLeftButtonDown += (sender, e) => { this.DragMove(); };
+             
+        }
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowState = WindowState.Minimized;
+            //ShowInTaskbar = false;
+        }
+        /// <summary>
+        /// 最大化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_max_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            ShowOrhide();
+        }
+        /// <summary>
+        /// 正常大小
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_normal_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            ShowOrhide();
+        }
+        /// <summary>
+        /// 最小化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 双击最大化或者是还原
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+                ShowOrhide();
+            }
+        }
+        /// <summary>
+        /// 显示或者隐藏按钮
+        /// </summary>
+        private void ShowOrhide()
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+
+                    break;
+                case WindowState.Minimized:
+                    break;
+                case WindowState.Maximized:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+      
+      
+    }
+}
---
+++ MainApp/Views/UcMain.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainApp.Views
+{
+    /// <summary>
+    /// UcMain.xaml 的交互逻辑
+    /// </summary>
+    public partial class UcMain : UserControl
+    {
+        public UcMain()
+        {
+            InitializeComponent();
+        }
+
+        #region 开始按钮改变格式
+        private void btnStart_MouseEnter(object sender, MouseEventArgs e)
+        {
+            pathStop.Visibility = Visibility.Hidden;
+            pathStart.Visibility = Visibility.Visible;
+        }
+        private void btnStart_MouseLeave(object sender, MouseEventArgs e)
+        {
+            pathStop.Visibility = Visibility.Visible;
+            pathStart.Visibility = Visibility.Hidden;
+        }
+        #endregion
+
+        /// <summary>
+        /// 开始按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
